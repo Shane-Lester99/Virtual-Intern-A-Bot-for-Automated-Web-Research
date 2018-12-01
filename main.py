b458356c6b1b1637fce9a065504c97de82d2a93d
shane <shanejlester@gmail.com>
2018-12-01 18:37:03 +0000

    Add CreateAnalysis query from main interface, fully functional/ tested
    CreateAnalysis creates a sentiment report. Note that fake data is still used.
    However at this point the query has been tested and seems to be working well.

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ import data_collector
 import query
 # TODO: Validate email. Decide if there are password rules. Decide how user retrieves password if they lose it.
 # Decide how this will be done how far I will go
-def is_valid_email(email):
+def isValidEmail(email):
     if email == "":
+        return False
+    return True
+
+# TODO: Make better check auth key function
+def isValidAuthKey(authKey):
+    return True
+
+# TODO: Make better auth link function
+def authenticateLink(link):
+    if link == "":
         return False
     return True
 
@@ -11,25 +21,27 @@ def main():
     # Create Query object. 
     queryMaster = query.Query()
     # In API, user entered email address
-    email = input("Please enter your email address or any unique username.")
-    while is_valid_email(email) is False:
-        email = input("Invalid email, please try again.")
+    email = input("Please enter your email address or any unique username: ")
+    while isValidEmail(email) is False:
+        email = input("Invalid email, please try again: ")
     # Email Does exist, enter it, enter password 
     if queryMaster.doesAccountExist(email):
-        password = input("Account exists, please enter your password.") 
+        password = input("Account exists, please enter your password: ") 
         i = 0
         while (queryMaster.authenticate(email, password) is False):
             if i == 2:
                 print("Three invalid log in attempt. Exiting program. Goodbye.")
                 return
-            password = input("Please enter your password.")
+            password = input("Invalid attempt. Please enter your password: ")
             i += 1
     else:
-        ans = input("Account does not exist. Would you like to create an account? (Y/N).")
+        ans = input("Account does not exist. Would you like to create an account? (Y/N): ")
         if ans is "Y" or ans is "y":
             # Allow user to create an account
-            password = input("Please enter password.")
-            queryMaster.createAccount(email, password)
+            password = input("Please enter password: ")
+            authKey = input("Please enter your auth key: ")
+            # TODO: Enter auth key validation here
+            queryMaster.createAccount(email, password, authKey)
         elif ans is "N" or ans is "n":
             print("Answer was no. Exiting program. Goodbye.")
             return
@@ -41,11 +53,33 @@ def main():
     userData = [email, password]
 
     while True:
-        command = input("Please enter a command.")
+        command = input("Please enter a command: ")
         if (command == 'Help'):
             print("Possible commands: \nHelp, \nCreateAnalysis, \nRetrievePossibleAnalysis, \nRetreieveSpecificAnalysis, \nDeleteSpecificAnalysis, \nDeleteAccount, \nUpdateAnalysis, \nQuit.")
         elif (command == "CreateAnalysis"):
             print("create analysis presses")
+            # Enter search string
+            searchString = input("Please enter a string to search: ")
+            # Enter list of reference links (1-3), authenticate them
+            howManyRefLinks = input("Please enter how many reference links you would like to enter (1-3 per query): ")
+            while str.isdigit(howManyRefLinks) is False or int(howManyRefLinks) > 3 or int(howManyRefLinks) < 1:   
+                howManyRefLinks = input("Not a number between 1 and 3. Please try again: ")
+                # TODO: Auth links to make sure they are valid websites before we call API 
+            print("Please enter reference links")
+            listOfRefLinks = []
+            for i in range(0, int(howManyRefLinks), 1):
+                link = input("Reference link " + str(i + 1) + ": ")
+                if authenticateLink(link) is False:
+                     link = input("False link. Please try again.\nReference link " + str(i + 1) + ": ")
+                listOfRefLinks.append(link)        
+            # Enter how many hit links you would like back (1-10)
+            howManyHitLinks = input("Please enter how many hit links would you like to get back to be analyzed (1-10 per query): ")
+            while str.isdigit(howManyHitLinks) is False or int(howManyHitLinks) > 10 or int(howManyHitLinks) < 1:   
+                howManyHitLinks = input("Not a number between 1 and 10. Please try again: ")
+            # Call Data_Collector.Collect_Data to return the data retrieval object
+            collectData = data_collector.Collect_Data(listOfRefLinks, searchString, int(howManyHitLinks))
+            # Pass the Data_Retrieval object to the CreateAnalysis function in the Query clss. 
+            queryMaster.createNewQuery(userData[0], collectData.data_retrieval)
         elif (command == "RetrievePossibleAnalysis"):
             print("retreive possible analyisis pressed")
         elif (command == "RetrieveSpecificAnalysis"):
--- query.py
+++ query.py
@@ -14,10 +14,10 @@ class Query(object):
     def __init__(self):
         self.__currentDB = "CollectiveConscience"
         self.__use()
-        self.cursor = mydb.cursor()
+        self.cursor = mydb.cursor(buffered = True)
     def doesAccountExist(self, email):    
-        st = ("SELECT @maxID := COUNT(*) FROM User WHERE Email = %s;")
-        self.cursor.execute(st, (email,))
+        query = ("SELECT @maxID := COUNT(*) FROM User WHERE Email = %s;")
+        self.cursor.execute(query, (email,))
         row = self.cursor.fetchone()
         if (row[0] == 1):
             return True
@@ -25,43 +25,49 @@ class Query(object):
             return False
     # Given an email and a password, this method will check if there is a match
     def authenticate(self, email, password): 
-        st = ("SELECT @maxID := COUNT(*) FROM User WHERE Email = %s and Password = %s;")
-        self.cursor.execute(st, (email, password))
+        query = ("SELECT @maxID := COUNT(*) FROM User WHERE Email = %s and Password = %s;")
+        self.cursor.execute(query, (email, password))
         row = self.cursor.fetchone()
         if (row[0] == 1):
             return True
         else:
             return False
     # This will enter a new user into our database
-    def enterNewUser(self, email, password):
+    def createAccount(self, email, password, authKey):
         if (self.doesAccountExist(email) is False):  
-            st = ("INSERT INTO User (Email, Password) VALUES (%s, %s);");
-            self.cursor.execute(st, (email, password))
+            query = ("INSERT INTO User (Email, Password, authKey) VALUES (%s, %s, %s);");
+            self.cursor.execute(query, (email, password, authKey))
             print("Account " + email + " has been sucessfully entered into db")
             return True
         else:
             print("Account " + email + " already exists.")
 
     # This will store a query from a data retrieval object
-    def createNewQuery(self, email, dataObject):
-        pass
-        #searchString = dataobject.search_string
-        #rl = dataObject.reference_links
-        #if (searchString is None or rl is None):
-        #    return
-        #self.cursor.execute("SET @email = " + email + ";")
-        #self.cursor.execute("SET @searchString = " + searchString + ";");
+    def createNewQuery(self, email, dataObject):  
+        if (dataObject.search_string is None or dataObject.reference_links is None):
+            return
         # This will create a new query with the new search string. That will create a new 'highest' query id.
-        #self.cursor.execute("INSERT INTO Query (SearchString) VALUES (@" + searchString");")
-        #self.cursor.execute("SELECT @queryID :=  MAX(QueryID) FROM Query;")
-        #self.cursor.execute("INSERT INTO QueryTable(Email, QueryID) VALUES (@email, @queryID);")
-        #for i in range(0, len(rl), 1): 
-            #self.cursor.execute("SET @rl = " + rl[i].url + ";")
-            #self.cursor.execute("INSERT INTO ReferenceLink (Url) VALUES (@rl)");
-            #self.cursor.execute("SELECT @referenceLinkID1 :=  MAX(ReferenceLinkID) FROM ReferenceLink;")
-            #self.cursor.execute("INSERT INTO ReferenceLinkTable(QueryID, ReferenceLinkID) VALUES (@queryID, @referenceLinkID1);")
-            #for j in range(0, len(rl[i].hit_links, 1):
-             #   self.cursor.execute(
+        query = "INSERT INTO Query (SearchString) VALUES (%s);"
+        self.cursor.execute(query, (dataObject.search_string,))
+        self.cursor.execute("SELECT @queryID :=  MAX(QueryID) FROM Query;")
+        query = ("INSERT INTO QueryTable(Email, QueryID) VALUES (%s, @queryID);")
+        self.cursor.execute(query, (email,))
+        rl = dataObject.reference_links
+        for i in range(0, len(rl), 1):
+            query = "INSERT INTO ReferenceLink (Url) VALUES (%s)";
+            self.cursor.execute(query, (rl[i].url,))
+            self.cursor.execute("SELECT @referenceLinkId :=  MAX(ReferenceLinkID) FROM ReferenceLink;")
+            self.cursor.execute("INSERT INTO ReferenceLinkTable(QueryID, ReferenceLinkID) VALUES (@queryID, @referenceLinkId);")
+            for j in range(0, len(rl[i].hit_links), 1):
+                query = "INSERT INTO HitLink (Url, RawContent, RunSentiment, RunWhere, RunWho, RunWhat, RunSummary) VALUES (%s, %s, %s, %s, %s, %s, %s);"
+                data = dataObject.reference_links[i].hit_links[j]
+                para = (data.url,  data.raw_content, data.run_object.sentiment_analysis, data.run_object.questions.where, data.run_object.questions.who, data.run_object.questions.what, data.run_object.summary)
+                self.cursor.execute(query, para)
+                self.cursor.execute("SELECT @hitLinkId :=  MAX(HitLinkID) FROM HitLink;")
+                self.cursor.execute("INSERT INTO HitLinkTable (HitLinkID, ReferenceLinkID) VALUES (@hitLinkId, @referenceLinkId)")
+                
+
+                
 
     def __use(self):
         return "USE DATABASE " + self.__currentDB
